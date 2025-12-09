d1f6491880d7dd5618a7021ef1df6b5514a86ebc
Akanksha-ambitious <akankshabhagat54@gmail.com>
2025-12-09 18:41:43 +0530

    Fix Cannot GET / and add root route

--- server.js
+++ server.js
@@ -26,4 +26,5 @@ app.post("/api/user", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
